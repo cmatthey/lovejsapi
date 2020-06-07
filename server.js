db5bd44506157114d4bac718f364bcb014c58f8a
Coco <coco>
2020-06-07 16:24:02 -0700

    Cleanup

--- server.js
+++ server.js
@@ -4,13 +4,12 @@
 // call the packages we need
 const express = require("express");
 const bodyParser = require("body-parser");
-const app = express();
 const morgan = require("morgan");
 const get_tweet = require("./get_tweet");
 
+const app = express();
 // configure app
 app.use(morgan("dev")); // log requests to the console
-
 // configure body parser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -86,5 +85,4 @@ app.use("/api", router);
 
 // START THE SERVER
 // =============================================================================
-app.listen(port);
-console.log("Magic happens on port " + port);
+app.listen(port, () => console.log(`API server is started on port ${port}`));
