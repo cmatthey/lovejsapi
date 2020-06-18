140bb8b3162c6dffa3a960388ade08b4e89cd040
Coco <coco>
2020-06-18 15:30:32 -0700

    Cleanup

--- db/index.js
+++ db/index.js
@@ -1,12 +1,7 @@
-// require("dotenv").config();
 const mongoose = require("mongoose");
 
-// if (process.env.NODE_ENV !== "production") {
-//   require("dotenv").config();
-// }
 mongoose
   .connect(process.env.MONGO_URL, { useNewUrlParser: true })
-  // .connect("mongodb://127.0.0.1:27017/test", { useNewUrlParser: true })
   .catch((e) => {
     console.error("Connection error", e.message);
   });
